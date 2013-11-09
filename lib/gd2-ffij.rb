77ad4d0e663d42f1087bde465615c4f578a50b9e
dark-panda <dark.panda@gmail.com>
2013-11-08 20:03:07 -0500

    Switch from GPL-2 to combination public domain/MIT license.

--- lib/gd2-ffij.rb
+++ lib/gd2-ffij.rb
@@ -1,25 +1,6 @@
 # encoding: ASCII-8BIT
 #
-# Ruby/GD2 -- Ruby binding for gd 2 graphics library
-#
-# Copyright 2005-2006 Robert Leslie, 2010-2013 J Smith
-#
-# This file is part of Ruby/GD2.
-#
-# Ruby/GD2 is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
+# See COPYRIGHT for license details.
 
 require 'ffi'
 require 'rbconfig'
